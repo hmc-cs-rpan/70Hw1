a1822b8e23a88138456ef6cdc9f8b20a37c8d137
Ricky Pan <rpan@cs.hmc.edu>
2015-09-08 12:01:36 -0700

    completed palindromes.cpp

--- palindromes/palindrome.cpp
+++ palindromes/palindrome.cpp
@@ -20,23 +20,44 @@ using namespace std;
  * \returns true iff `text` is a palindrome (same backwards and forwards).
  *
  * \remarks
- *   Unfortunately, we currently literally require the characters in
- *   the string to be the same backwards and forwards, rather than
- *   only caring about the letters.
+ *   isPalindrome ignores capitalization and punctuation and spaces
  *
- * \todo Ignore Spaces and punctuation
- * \todo Ignore capitalization
  */
+ int START_OF_NUMERALS = 48;                // ASCII values for characters
+ int END_OF_NUMERALS = 57;                  // we used these to classify characters
+ int START_OF_CAPITALS = 65;                // as numerals, uppercase, and lowercase
+ int END_OF_CAPITALS = 90;
+ int START_OF_LOWERCASE = 97;
+ int END_OF_LOWERCASE = 122;
+ int LOWER_TO_UPPER_DIFF = 32;              // 32 is the difference in ASCII values between a lowercase character and 
+                                            // its corresponding uppercase character
 bool isPalindrome(string text /**< Canditate palindrome.*/)
 {
+    string noPunct = "";                    // string in which we will store a modified version of the input string without punctuation 
+                                            // and with lowercase characters
+    for(int i = 0; i < text.length(); i++)  
+    {
+        char charAtI = text[i];             // retreives the character at index i
+        int asciiVal = charAtI;             // converts the character into its ASCII value
+        if((asciiVal >= START_OF_NUMERALS && asciiVal <= END_OF_NUMERALS) ||    // checks if character is a number
+            (asciiVal >= START_OF_LOWERCASE && asciiVal <= END_OF_LOWERCASE))   // checks if character is lowercase
+        {
+            noPunct += charAtI;             // if so, add the character to noPunct
+        }
+        else if(asciiVal >= START_OF_CAPITALS && asciiVal <= END_OF_CAPITALS)   // checks if character is uppercase
+        {
+            char lowercase = asciiVal + LOWER_TO_UPPER_DIFF;     // if so, use ASCII values to convert to lowercase  
+            noPunct += lowercase;           // now add the character to noPunct
+        }
+    }
     int left  = 0;                      // Indexes of the leftmost and
-    int right = text.length() - 1;      // rightmost unchecked character.
+    int right = noPunct.length() - 1;      // rightmost unchecked character.
 
     // Check palindrome by moving left and right boundaries progressively
     // closer until they finally meet or pass each other.  Bail early if
     // we find a mismatch.
     while (left < right) {
-        if (text[left] != text[right])
+        if (noPunct[left] != noPunct[right])
             return false;
 
         ++left;
